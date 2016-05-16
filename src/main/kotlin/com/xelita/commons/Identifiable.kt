9d2e4942f4370207ba3437849a5680b6346a9ae7
xelita <admin@xelita.com>
2016-05-16 10:52:08 +0200

    @JsonIgnore on getId and getType fields

--- src/main/kotlin/com/xelita/commons/Identifiable.kt
+++ src/main/kotlin/com/xelita/commons/Identifiable.kt
@@ -1,5 +1,7 @@
 package com.xelita.commons;
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 /**
  * Define an identifiable object.
  *
@@ -12,6 +14,7 @@ interface Identifiable {
      *
      * @return String
      */
+    @JsonIgnore
     fun getId(): String;
 
     /**
@@ -19,5 +22,6 @@ interface Identifiable {
      *
      * @return String
      */
+    @JsonIgnore
     fun getType(): String;
 }
